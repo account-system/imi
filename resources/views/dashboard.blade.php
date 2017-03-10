a4c4e0fdb41cb23cfcafdd1a0f5bac75914e5d5d
Chomnan Im <chomnan.im@yahoo.com>
2017-03-10 15:39:47 +0700

    commit imi project

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('header')
+    <section class="content-header">
+      <h1>Dashboard
+        <small>The first page you see after login</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="{{ url('') }}">{{ config('app.name') }}</a></li>
+        <li class="active">Dashboard</li>
+      </ol>
+    </section>
+@endsection
+
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box box-default">
+                <div class="box-header with-border">
+                    <div class="box-title">Login status</div>
+                </div>
+
+                <div class="box-body">The first page you see after login</div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/inc/sidebar.blade.php
@@ -0,0 +1,41 @@
+@if (Auth::check())
+    <!-- Left side column. contains the sidebar -->
+    <aside class="main-sidebar">
+      <!-- sidebar: style can be found in sidebar.less -->
+      <section class="sidebar">
+        <!-- Sidebar user panel -->
+        <div class="user-panel">
+          <div class="pull-left image">
+            <img src="https://placehold.it/160x160/00a65a/ffffff/&text={{ mb_substr(Auth::user()->name, 0, 1) }}" class="img-circle" alt="User Image">
+          </div>
+          <div class="pull-left info">
+            <p>{{ Auth::user()->name }}</p>
+            <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+          </div>
+        </div>
+        <!-- sidebar menu: : style can be found in sidebar.less -->
+        <ul class="sidebar-menu">
+          <li class="header">Administration</li>
+          <!-- ================================================ -->
+          <!-- ==== Recommended place for admin menu items ==== -->
+          <!-- ================================================ -->
+          <li><a href="{{ url('').'/dashboard' }}"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
+
+
+          <!-- ======================================= -->
+          <li class="header">User</li>
+          <li>
+            <a href="{{ url('').'/logout' }}" onclick="event.preventDefault();
+              document.getElementById('logout-form').submit();">
+              <i class="fa fa-btn fa-sign-out"></i><span>Logout</span>
+            </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                {{ csrf_field() }}
+            </form>  
+          </li>
+        </ul>
+      </section>
+      <!-- /.sidebar -->
+    </aside>
+@endif
