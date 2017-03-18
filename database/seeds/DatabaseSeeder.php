5343557fa2548ca871987eb679f6c73dd8251311
Chomnan Im <chomnan.im@yahoo.com>
2017-03-18 11:50:03 +0700

    commit database/seeds/DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(AccountTypesTableSeeder::class);
+        //$this->call(AccountTypesTableSeeder::class);
+        $this->call(MasterTypeDetailsTableSeeder::class);
     }
 }
